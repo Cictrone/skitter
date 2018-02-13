f27056a5aa7421e9f8ad36d66238e92ab5c1ee47
Nicholas O'Brien <ndo9903@rit.edu>
2018-02-13 12:42:23 -0500

    made the test actually useful

--- tests.py
+++ tests.py
@@ -1,10 +1,14 @@
 import unittest
+import requests
 
 
 class TestMethods(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(":)", ":)")
-
+    def test_IsAuthenticated_Fail(self):
+        headers = {"Host": "skitter", "sessionID": "deadbeef"}
+        resp = requests.post("http://skitter/isAuthenticated", headers=headers)
+        # test assumes sessionID will never be deadbeef
+        self.assertTrue(resp.status_code == 401)
+        
 
 if __name__ == '__main__':
     unittest.main()
