0cef372f61a2a9ae11dac759ee1ea3d7d9af594e
Nicholas O'Brien <ndo9903@rit.edu>
2018-02-13 12:53:19 -0500

    fixed test to fail on connection error (if server is down)

--- tests.py
+++ tests.py
@@ -5,10 +5,14 @@ import requests
 class TestMethods(unittest.TestCase):
     def test_IsAuthenticated_Fail(self):
         headers = {"Host": "skitter", "sessionID": "deadbeef"}
-        resp = requests.post("http://skitter/isAuthenticated", headers=headers)
+        try:
+            resp = requests.post("http://skitter/isAuthenticated", headers=headers)
+        except requests.exceptions.ConnectionError as e:
+            # if server is not up
+            self.fail("Failed with {}".format(e))
         # test assumes sessionID will never be deadbeef
         self.assertTrue(resp.status_code == 401)
-        
+
 
 if __name__ == '__main__':
     unittest.main()
