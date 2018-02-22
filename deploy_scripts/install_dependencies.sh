91a791bb5eba27938676ad7458269e27c588d681
Nicholas O'Brien <ndo9903@rit.edu>
2018-02-21 20:55:40 -0500

    forgot the damn -y for the package install :/

--- deploy_scripts/install_dependencies.sh
+++ deploy_scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Installing Python/Pip Dependencies on EC2 Instance..."
-apt-get install python3-pip
-apt-get install python3
+apt-get -y install python3-pip
+apt-get -y install python3
 pip3 install -r /home/ubuntu/skitter/requirements.txt
