3cd7c48b0921b0863fdb3dcec9aaea019ea0d9b4
Nicholas O'Brien <ndo9903@rit.edu>
2018-02-21 22:50:28 -0500

    changing firwall rules and such

--- deploy_scripts/install_dependencies.sh
+++ deploy_scripts/install_dependencies.sh
@@ -3,3 +3,5 @@ echo "Installing Python/Pip Dependencies on EC2 Instance..."
 apt-get -y install python3-pip
 apt-get -y install python3
 pip3 install -r /home/ubuntu/skitter/requirements.txt
+iptables -A INPUT -p tcp -m multiport --dports 80,443,5000 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --dports 80,443,5000 -m conntrack --ctstate ESTABLISHED -j ACCEPT
--- deploy_scripts/start_server.sh
+++ deploy_scripts/start_server.sh
@@ -2,3 +2,5 @@
 echo "Starting Flask on EC2 Instance..."
 export FLASK_APP=/home/ubuntu/skitter/app.py
 /usr/local/bin/flask run --host=0.0.0.0 >/dev/null 2>&1 &
+
+sleep 5
