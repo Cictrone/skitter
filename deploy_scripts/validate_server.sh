0237a21ce51b3236575ef870253c3c3e4a9b82e2
Nicholas O'Brien <ndo9903@rit.edu>
2018-02-21 22:53:46 -0500

    added some verbosity to validation

--- deploy_scripts/validate_server.sh
+++ deploy_scripts/validate_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 status_code=$(curl --silent --head 0.0.0.0:5000/ | grep HTTP/ | awk -F ' ' '{ print $2 }')
-
+echo $status_code
+echo $(ps aux | grep /usr/local/bin/flask | grep -v grep | awk '{print $2}')
 if [[ $status_code == "200" ]]; then
     exit 0
 else
