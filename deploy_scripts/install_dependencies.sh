720bc24a6a855e5995ba04775c9871af05a8c2da
Nicholas O'Brien <ndo9903@rit.edu>
2018-02-21 20:52:11 -0500

    testing deploy enivroment5

--- deploy_scripts/install_dependencies.sh
+++ deploy_scripts/install_dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "Installing Pip Dependencies on EC2 Instance..."
-ls -la /home/ubuntu/skitter/
+echo "Installing Python/Pip Dependencies on EC2 Instance..."
+apt-get install python3-pip
+apt-get install python3
 pip3 install -r /home/ubuntu/skitter/requirements.txt
--- deploy_scripts/start_server.sh
+++ deploy_scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting Flask on EC2 Instance..."
-python3 ../app.py &
+python3 /home/ubuntu/skitter/app.py &
