52fb2a11f2ad36e0f29298520deddaea0d23ceac
Nicholas O'Brien <ndo9903@rit.edu>
2018-02-21 20:47:29 -0500

    testing deploy enivroment4

--- deploy_scripts/install_dependencies.sh
+++ deploy_scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Installing Pip Dependencies on EC2 Instance..."
-ls -la home/ubuntu/skitter/
-pip3 install -r home/ubuntu/skitter/requirements.txt
+ls -la /home/ubuntu/skitter/
+pip3 install -r /home/ubuntu/skitter/requirements.txt
