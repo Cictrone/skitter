1ebe7519bc3e11e5dcaffcc90c271192e822a313
Nicholas O'Brien <ndo9903@rit.edu>
2018-02-21 23:33:38 -0500

    tried new distro on ec2 and changed setup

--- deploy_scripts/install_dependencies.sh
+++ deploy_scripts/install_dependencies.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "Installing Python/Pip Dependencies on EC2 Instance..."
-apt-get -y install python3-pip
-apt-get -y install python3
-pip3 install -r /home/ubuntu/skitter/requirements.txt
+yum -y update
+yum install epel-release -y
+yum install ruby -y
+yum install python35 -y
+yum install python35-pip -y
+pip-3.5 install -r /home/ubuntu/skitter/requirements.txt
 iptables -A INPUT -p tcp -m multiport --dports 80,443,5000 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -m multiport --dports 80,443,5000 -m conntrack --ctstate ESTABLISHED -j ACCEPT
