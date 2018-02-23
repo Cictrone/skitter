733aab7122564556f0591cd107337c3dabe90819
Nicholas O'Brien <ndo9903@rit.edu>
2018-02-23 10:13:02 -0500

    fixed minor permissions bug

--- deploy_scripts/start_server.sh
+++ deploy_scripts/start_server.sh
@@ -3,6 +3,9 @@ echo "Starting Flask on EC2 Instance..."
 cd /opt/skitter/
 sudo /usr/local/bin/docker-compose build
 sudo mkdir -p /var/log/skitter/docker
-sudo /usr/local/bin/docker-compose up >/var/log/skitter/docker/stdout 2>/var/log/skitter/docker/stderr &
+sudo touch /var/log/skitter/docker/stdout
+sudo touch /var/log/skitter/docker/stderr
+sudo chown -R ec2-user:ec2-user /var/log/skitter/
+sudo /usr/local/bin/docker-compose up >> /var/log/skitter/docker/stdout 2>> /var/log/skitter/docker/stderr &
 
 sleep 10
