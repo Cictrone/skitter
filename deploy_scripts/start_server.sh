68c56c2310ed04d08722d9d164a40e57e4adc013
Nicholas O'Brien <ndo9903@rit.edu>
2018-02-22 23:02:56 -0500

    switched from flask app to docker (better for future)

--- deploy_scripts/start_server.sh
+++ deploy_scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting Flask on EC2 Instance..."
-export FLASK_APP=/home/ubuntu/skitter/app.py
-/usr/local/bin/flask run --host=0.0.0.0 >/dev/null 2>&1 &
+cd /home/ubuntu/skitter/
+docker-compose up >/dev/null 2>&1 &
 
-sleep 5
+sleep 10
--- deploy_scripts/validate_server.sh
+++ deploy_scripts/validate_server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-status_code=$(curl --silent --head 0.0.0.0:5000/ | grep HTTP/ | awk -F ' ' '{ print $2 }')
-echo $status_code
-echo $(ps aux | grep /usr/local/bin/flask | grep -v grep | awk '{print $2}')
+status_code=$(curl --silent --head 0.0.0.0:8080/ | grep HTTP/ | awk -F ' ' '{ print $2 }')
 if [[ $status_code == "200" ]]; then
     exit 0
 else
