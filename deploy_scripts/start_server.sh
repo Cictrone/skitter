b02403ee21e5f3f8773c52821460ed45ad2f80cd
Nicholas O'Brien <ndo9903@rit.edu>
2018-03-01 23:44:44 -0500

    reseparatedthe docker builds and ups

--- deploy_scripts/start_server.sh
+++ deploy_scripts/start_server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "Starting Flask on EC2 Instance..."
 cd /opt/skitter/
+sudo /usr/local/bin/docker-compose build
 sudo mkdir -p /var/log/skitter/docker
 sudo touch /var/log/skitter/docker/stdout
 sudo touch /var/log/skitter/docker/stderr
 sudo chown -R ec2-user:ec2-user /var/log/skitter/
-sudo /usr/local/bin/docker-compose up --build >> /var/log/skitter/docker/stdout 2>> /var/log/skitter/docker/stderr &
+sudo /usr/local/bin/docker-compose up >> /var/log/skitter/docker/stdout 2>> /var/log/skitter/docker/stderr &
 
 sleep 10
