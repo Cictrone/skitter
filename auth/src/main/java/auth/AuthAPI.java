32cbb1163172c4ffc4b88b74127d61b7c69bc43b
Nicholas O'Brien <ndo9903@rit.edu>
2018-03-02 13:37:22 -0500

    fixed the way spring handles request so it better to generalize and added paths for this milestone into nginx

--- auth/src/main/java/auth/AuthAPI.java
+++ auth/src/main/java/auth/AuthAPI.java
@@ -4,19 +4,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Collections;
-import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class AuthAPI{
 
   @RequestMapping(path="/isAuthenticated", method=RequestMethod.POST)
-  public Map isAuthenticated(HttpServletRequest request, HttpServletResponse response) {
+  public ResponseEntity<Map> isAuthenticated(HttpServletRequest request, HttpServletResponse response) throws URISyntaxException {
     // TODO: add logic
-    return Collections.singletonMap("response", true);
+    String location = request.getHeader("Host")+request.getContextPath();
+    HttpHeaders headers = new HttpHeaders();
+    headers.setLocation(new URI(location));
+    Map json_resp = Collections.singletonMap("response", false);
+    return new ResponseEntity<Map>(json_resp, headers, HttpStatus.UNAUTHORIZED);
   }
 
 }
--- tests/tests.py
+++ tests/tests.py
@@ -1,20 +1,29 @@
 import unittest
 import requests
+import json
 
 
 class TestMethods(unittest.TestCase):
     def test_IsAuthenticated_Fail(self):
-        headers = {"Host": "skitter", "sessionID": "deadbeef"}
+        headers = {"Host": "localhost", "sessionID": "deadbeef"}
         try:
             resp = requests.post("http://localhost/isAuthenticated", headers=headers)
         except requests.exceptions.ConnectionError as e:
-            return True
             # if server is not up
             self.fail("Failed with {}".format(e))
         # test assumes sessionID will never be deadbeef
-        return True
         self.assertTrue(resp.status_code == 401)
-
+        
+    def test_IsAuthenticated_Fail_Reps(self):
+        headers = {"Host": "localhost", "sessionID": "deadbeef"}
+        try:
+            resp = requests.post("http://localhost/isAuthenticated", headers=headers)
+        except requests.exceptions.ConnectionError as e:
+            # if server is not up
+            self.fail("Failed with {}".format(e))
+        # test assumes sessionID will never be deadbeef
+        resp_parsed = resp.json()
+        self.assertTrue(resp_parsed['response'] == False)
 
 if __name__ == '__main__':
     unittest.main()
