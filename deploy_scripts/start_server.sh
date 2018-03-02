6707a1628811e3e417cdc182d6a10a65a5b1239b
Nicholas O'Brien <ndo9903@rit.edu>
2018-03-02 12:02:32 -0500

    changed docker-compose to be use scale and installed php :)

--- deploy_scripts/start_server.sh
+++ deploy_scripts/start_server.sh
@@ -6,6 +6,6 @@ sudo mkdir -p /var/log/skitter/docker
 sudo touch /var/log/skitter/docker/stdout
 sudo touch /var/log/skitter/docker/stderr
 sudo chown -R ec2-user:ec2-user /var/log/skitter/
-sudo /usr/local/bin/docker-compose up >> /var/log/skitter/docker/stdout 2>> /var/log/skitter/docker/stderr &
+sudo /usr/local/bin/docker-compose up --scale skitter=2 >> /var/log/skitter/docker/stdout 2>> /var/log/skitter/docker/stderr &
 
 sleep 10
