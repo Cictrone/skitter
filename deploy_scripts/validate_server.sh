e89a31fbf478db3649d1085054c35cf3d766976a
Nicholas O'Brien <ndo9903@rit.edu>
2018-03-09 01:38:50 -0500

    changed AWS validate service to retry connecting 5 times before truly failing

--- deploy_scripts/validate_server.sh
+++ deploy_scripts/validate_server.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+n=0
+until [ $n -ge 5 ]
+do
+  (curl -k --silent --head https://0.0.0.0/ && break) || true
+  n=$[$n+1]
+  sleep 5
+done
 status_code=$(curl -k --silent --head https://0.0.0.0/ | grep HTTP/ | awk -F ' ' '{ print $2 }')
 if [[ $status_code == "200" ]]; then
     exit 0
