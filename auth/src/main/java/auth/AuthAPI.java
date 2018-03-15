7500672ac42376ee3f863fc5bfede5ff15d08aa7
Nicholas O'Brien <ndo9903@rit.edu>
2018-03-15 18:08:37 -0400

    setup initial API endpoints and tests, will need to be updated later when skits are implemented

--- auth/src/main/java/auth/AuthAPI.java
+++ auth/src/main/java/auth/AuthAPI.java
@@ -8,6 +8,7 @@ import org.springframework.web.util.WebUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import com.unboundid.ldap.sdk.LDAPException;
 import java.security.GeneralSecurityException;
@@ -27,6 +28,73 @@ import java.nio.charset.Charset;
 public class AuthAPI {
 
 
+  @RequestMapping(path="/RegisterUser", method=RequestMethod.POST)
+  public ResponseEntity<Map> RegisterUser(HttpServletRequest request, HttpServletResponse response) throws Exception{
+    final String username = request.getParameter("username");
+    final String password = request.getParameter("password");
+    final String displayName = request.getParameter("displayName");
+    final String email = request.getParameter("email");
+    HttpStatus responseStatus = HttpStatus.UNAUTHORIZED;
+    Boolean loginResponse = false;
+    Boolean registerSuccess = false;
+    if (username != null && password != null && displayName != null && email != null) {
+        AuthClient client = new AuthClient(username, password, false);
+        loginResponse = client.loginSuccess;
+        if(loginResponse){
+          registerSuccess = client.RegisterUser(displayName, email);
+          // add more logic for skits
+          if(registerSuccess){
+            responseStatus = HttpStatus.CREATED;
+          }else{
+            // if authenticated BUT account already exists
+            responseStatus = HttpStatus.CONFLICT;
+          }
+        }
+    }
+
+    final String location = request.getHeader("Host")+request.getContextPath();
+    HttpHeaders headers = new HttpHeaders();
+    headers.setLocation(new URI(location));
+    headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+    Map<String, String> json_resp = new HashMap<>();
+    json_resp.put("response", String.valueOf(registerSuccess));
+    return new ResponseEntity<Map>(json_resp, headers, responseStatus);
+  }
+
+  @RequestMapping(path="/DeleteUser", method=RequestMethod.POST)
+  public ResponseEntity<Map> DeleteUser(HttpServletRequest request, HttpServletResponse response) throws Exception{
+    HttpStatus responseStatus = HttpStatus.UNAUTHORIZED;
+    Boolean loginResponse = false;
+    Boolean deleteResponse = false;
+    String sessionID = WebUtils.getCookie(request, "sessionID").getValue();
+    if(sessionID != null){
+      AuthClient client = new AuthClient(sessionID);
+      loginResponse = client.loginSuccess;
+      if(loginResponse){
+        deleteResponse = client.DeleteUser();
+        // add more logic for skits
+        if(deleteResponse){
+          responseStatus = HttpStatus.OK;
+        }
+      }
+    }
+    final String location = request.getHeader("Host")+request.getContextPath();
+    HttpHeaders headers = new HttpHeaders();
+    headers.setLocation(new URI(location));
+    headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+    Map<String, String> json_resp = new HashMap<>();
+    json_resp.put("response", String.valueOf(deleteResponse));
+    return new ResponseEntity<Map>(json_resp, headers, responseStatus);
+  }
+
+
+
+
+
+
+
+
+
   @RequestMapping(path="/isAuthenticated", method=RequestMethod.POST)
   public ResponseEntity<Map> isAuthenticated(HttpServletRequest request, HttpServletResponse response) throws Exception {
     HttpStatus responseStatus = HttpStatus.UNAUTHORIZED;
@@ -36,12 +104,13 @@ public class AuthAPI {
       AuthClient client = new AuthClient(sessionID);
       loginResponse = client.loginSuccess;
       if(loginResponse){
-        responseStatus = HttpStatus.ACCEPTED;
+        responseStatus = HttpStatus.OK;
       }
     }
     final String location = request.getHeader("Host")+request.getContextPath();
     HttpHeaders headers = new HttpHeaders();
     headers.setLocation(new URI(location));
+    headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
     Map<String, String> json_resp = new HashMap<>();
     json_resp.put("response", String.valueOf(loginResponse));
     return new ResponseEntity<Map>(json_resp, headers, responseStatus);
@@ -59,10 +128,10 @@ public class AuthAPI {
         final String[] values = credentials.split(":",2);
         final String username = values[0];
         final String password = values[1];
-        AuthClient client = new AuthClient(username, password);
+        AuthClient client = new AuthClient(username, password, true);
         loginResponse = client.loginSuccess;
         if(loginResponse){
-          responseStatus = HttpStatus.ACCEPTED;
+          responseStatus = HttpStatus.OK;
           sessionID = client.Login();
         }
     }
@@ -70,10 +139,11 @@ public class AuthAPI {
     final String location = request.getHeader("Host")+request.getContextPath();
     HttpHeaders headers = new HttpHeaders();
     headers.setLocation(new URI(location));
+    headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
     Map<String, String> json_resp = new HashMap<>();
     json_resp.put("response", String.valueOf(loginResponse));
     if(loginResponse){
-      json_resp.put("sessionID", sessionID);
+      headers.add("Set-Cookie", "sessionID="+sessionID+"; HttpOnly; Secure");
     }
     return new ResponseEntity<Map>(json_resp, headers, responseStatus);
   }
@@ -87,12 +157,13 @@ public class AuthAPI {
       AuthClient client = new AuthClient(sessionID);
       loginResponse = client.Logout();
       if(loginResponse){
-        responseStatus = HttpStatus.ACCEPTED;
+        responseStatus = HttpStatus.OK;
       }
     }
     final String location = request.getHeader("Host")+request.getContextPath();
     HttpHeaders headers = new HttpHeaders();
     headers.setLocation(new URI(location));
+    headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
     Map<String, String> json_resp = new HashMap<>();
     json_resp.put("response", String.valueOf(loginResponse));
     return new ResponseEntity<Map>(json_resp, headers, responseStatus);
--- auth/src/main/java/auth/AuthClient.java
+++ auth/src/main/java/auth/AuthClient.java
@@ -26,41 +26,33 @@ public class AuthClient{
   private BindResult bind;
   protected Boolean loginSuccess = false;
 
-  public AuthClient(String username, String password) throws Exception{
+  public AuthClient(String username, String password, Boolean checkLocalDB) throws Exception{
     this.username = username;
     this.password = password;
 
-    String query = "select displayName from User where email=?";
-    Class.forName("com.mysql.jdbc.Driver");
-    Connection conn = DriverManager.getConnection(this.authDB, "root", "skitter_auth_dbpass");
-    PreparedStatement preparedStmt = conn.prepareStatement(query);
-    preparedStmt.setString(1, this.username);
-    ResultSet rs = preparedStmt.executeQuery();
-    String test = null;
-    while (rs.next()) {
-				test = rs.getString("displayName");
-		}
-    if(test == null){
-      this.loginSuccess = false;
-    }else{
-      SSLUtil sslUtil = new SSLUtil(new TrustAllTrustManager());
-      SSLSocketFactory sslSocketFactory = sslUtil.createSSLSocketFactory();
-      LDAPConnection c = new LDAPConnection(sslSocketFactory);
-      c.connect(this.server, 636); // 636 is LDAPSSL
-      String bindDN = "uid="+username+this.ou_and_dcs;
-      try{
-        this.bind = c.bind(bindDN, password);
-        this.loginSuccess = true;
-      }catch(LDAPBindException e){
-        System.err.println("Login Failed for "+bindDN);
-
+    if(checkLocalDB){
+      this.loginSuccess = this.isUserInDB(this.username);
+      if(!this.loginSuccess){
+        return;
       }
     }
+    SSLUtil sslUtil = new SSLUtil(new TrustAllTrustManager());
+    SSLSocketFactory sslSocketFactory = sslUtil.createSSLSocketFactory();
+    LDAPConnection c = new LDAPConnection(sslSocketFactory);
+    c.connect(this.server, 636); // 636 is LDAPSSL
+    String bindDN = "uid="+username+this.ou_and_dcs;
+    try{
+      this.bind = c.bind(bindDN, password);
+      this.loginSuccess = true;
+    }catch(LDAPBindException e){
+      System.err.println("Login Failed for "+bindDN);
+    }
+
   }
 
   public AuthClient(String sessionID) throws Exception{
     this.sessionID = sessionID;
-    String query = "select displayName from User where sessionID=?";
+    String query = "select username from User where sessionID=?";
     Class.forName("com.mysql.jdbc.Driver");
     Connection conn = DriverManager.getConnection(this.authDB, "root", "skitter_auth_dbpass");
     PreparedStatement preparedStmt = conn.prepareStatement(query);
@@ -68,45 +60,115 @@ public class AuthClient{
     ResultSet rs = preparedStmt.executeQuery();
     this.username = null;
     while (rs.next()) {
-				this.username = rs.getString("displayName");
+				this.username = rs.getString("username");
 		}
     conn.close();
     if(this.username == null){
       this.loginSuccess = false;
+      System.out.println("User: "+this.username+" has unsuccessfully authenticated with a sessionID...");
     }else{
+      System.out.println("User: "+this.username+" has successfully authenticated with a sessionID...");
       this.loginSuccess = true;
     }
+  }
+
+  private Boolean isUserInDB(String username) throws Exception{
+    String query = "select email from User where username=?";
+    Class.forName("com.mysql.jdbc.Driver");
+    Connection conn = DriverManager.getConnection(this.authDB, "root", "skitter_auth_dbpass");
+    PreparedStatement preparedStmt = conn.prepareStatement(query);
+    preparedStmt.setString(1, this.username);
+    ResultSet rs = preparedStmt.executeQuery();
+    String test = null;
+    while (rs.next()) {
+        test = rs.getString("email");
+    }
+    conn.close();
+    if(test == null){
+      return false;
+    }
+    return true;
+  }
+
+  public Boolean RegisterUser(String displayName, String email) throws Exception{
+    // Should never happen
+    if(!this.loginSuccess){
+      System.out.println("User: "+this.username+" has failed to be registered...");
+      return false;
+    }
+    if(this.isUserInDB(this.username)){
+      System.out.println("User: "+this.username+" has failed to be registered twice...");
+      return false;
+    }
+    Class.forName("com.mysql.jdbc.Driver");
+    Connection conn = DriverManager.getConnection(this.authDB, "root", "skitter_auth_dbpass");
+    String query = "insert into User (username, displayName, email, profileImage) values(?,?,?,?)";
+    PreparedStatement preparedStmt = conn.prepareStatement(query);
+    preparedStmt.setString(1, this.username);
+    preparedStmt.setString(2, displayName);
+    preparedStmt.setString(3, email);
+    preparedStmt.setString(4, "http://laoblogger.com/images/default-profile-picture-5.jpg");
+    preparedStmt.execute();
+    conn.close();
+    System.out.println("User: "+this.username+" has successfully been registered...");
+    return true;
+  }
+
+  public Boolean DeleteUser() throws Exception{
+    // Should never happen
+    if(!this.loginSuccess){
+      System.out.println("User: "+this.username+" has failed to be deleted...");
+      return false;
+    }
+    if(!this.isUserInDB(this.username)){
+      System.out.println("User: "+this.username+" has failed to be deleted twice...");
+      return false;
+    }
+    Class.forName("com.mysql.jdbc.Driver");
+    Connection conn = DriverManager.getConnection(this.authDB, "root", "skitter_auth_dbpass");
+    String query = "delete from User where username=?";
+    PreparedStatement preparedStmt = conn.prepareStatement(query);
+    preparedStmt.setString(1, this.username);
+    preparedStmt.execute();
+    conn.close();
+    System.out.println("User: "+this.username+" has successfully been deleted...");
+    return true;
   }
 
   public String Login() throws Exception{
     // Should never happen
     if(!this.loginSuccess){
+      System.out.println("User: "+this.username+" has failed to be logged in...");
       return null;
     }
     Class.forName("com.mysql.jdbc.Driver");
     Connection conn = DriverManager.getConnection(this.authDB, "root", "skitter_auth_dbpass");
-    String query = "update User set sessionID=? where email=?";
+    String query = "update User set sessionID=? where username=?";
     PreparedStatement preparedStmt = conn.prepareStatement(query);
     this.sessionID = UUID.randomUUID().toString().replace("-", "");
     preparedStmt.setString(1, this.sessionID);
     preparedStmt.setString(2, this.username);
     preparedStmt.execute();
     conn.close();
+    System.out.println("User: "+this.username+" has successfully been logged in...");
     return this.sessionID;
   }
+
 
   public Boolean Logout() throws Exception{
     // Should never happen
     if(!this.loginSuccess){
+      System.out.println("User: "+this.username+" has failed to be logged out...");
       return false;
     }
     Class.forName("com.mysql.jdbc.Driver");
     Connection conn = DriverManager.getConnection(this.authDB, "root", "skitter_auth_dbpass");
-    String query = "update User set sessionID='' where email=?";
+    String query = "update User set sessionID=NULL where sessionID=?";
     PreparedStatement preparedStmt = conn.prepareStatement(query);
-    preparedStmt.setString(1, this.username);
+    preparedStmt.setString(1, this.sessionID);
     preparedStmt.execute();
     conn.close();
+    System.out.println("User: "+this.username+" has successfully been logged out...");
     return true;
   }
 
