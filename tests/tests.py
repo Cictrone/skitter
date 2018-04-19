1a46a5ebd429a819839618bad433780f62c638cc
Nicholas O'Brien <ndo9903@rit.edu>
2018-04-19 11:23:28 -0400

    removed erroneous test

--- tests/tests.py
+++ tests/tests.py
@@ -14,16 +14,6 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class TestMethods(unittest.TestCase):
 
-    def test_IsAuthenticated_Fail(self):
-        headers = {"Host": "localhost", "Cookie": "sessionID=deadbeef"}
-        try:
-            resp = requests.post("https://localhost/isAuthenticated", headers=headers, verify=False)
-        except requests.exceptions.ConnectionError as e:
-            # if server is not up
-            self.fail("Failed with {}".format(e))
-        # test assumes sessionID will never be deadbeef
-        resp_parsed = resp.json()
-        self.assertTrue(resp.status_code == 401 and resp_parsed['response'] == "false")
 
 class Browser():
     def __init__(self, driver):
